212c56ed389581649d756b534c8b558c5e62ba72
jdltechworks <jdltechworks@gmail.com>
2016-01-10 11:17:30 +0800

    New landing page

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
-fonts_dir = "fonts"
+fonts_dir = "css/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
