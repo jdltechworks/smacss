cfaea23eb31fd8d31660f331a57334e0658fc137
jdltechworks <jdltechworks@gmail.com>
2016-05-31 08:00:17 +0800

    Removing breakpoint module

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,73 @@
-var debug = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV ? true: false;
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  context: path.join(__dirname, "/"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components|dist)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
+if (!debug) {
+  console.log('WARNING: You are running this in production env');
+}
+
+function getBabelQuery () {
+  var queryString = '';
+  var query = {
+    presets: ['react', 'es2015', 'stage-0'],
+    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+  };
+  for (var queryKey in query) {
+    var queryList = query[queryKey];
+    for (var key in queryList) {
+      var item = queryList[key];
+      if (queryString) {
+        queryString += ','
       }
-    ]
+      queryString += queryKey + '[]=' + item;
+    }
+  }
+
+  return queryString;
+}
+
+module.exports = [
+  {
+    context: path.join(__dirname, "/"),
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: [
+      "./js/client.js",
+      "./assets/sass/index.scss"
+    ],
+    // externals: [
+    //   /images/
+    // ],
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components|dist)/,
+          loaders: ['react-hot', 'babel-loader?' + getBabelQuery()]
+        },
+        {
+          test: /(img|fonts|\.(png|jpg|eot|woff2|woff|ttf|svg))/,
+          loaders: ["react-hot", "file?name=" + (debug ? "css/" : "../") + "[path][name].[ext]&context=assets/"],
+        },
+        {
+          test: /\.scss$/,
+          loader: debug ? null : ExtractTextPlugin.extract(
+            'style',
+            'css!sass'
+          ),
+          loaders: debug ? ['style?sourceMap', 'css', 'sass'] : []
+        }
+      ]
+    },
+    output: {
+      path: __dirname + "/dist/css",
+      filename: "../js/client.min.js"
+    },
+    plugins: debug ? [] : [
+      new ExtractTextPlugin('../css/index.css'),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ],
   },
-  output: {
-    path: __dirname + "/dist/",
-    filename: "js/client.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
-};
+];
