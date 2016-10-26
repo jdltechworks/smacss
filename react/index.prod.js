ac71d275f0eab8620f80b394a1242a728869ea0a
jdltechworks <jose@bywave.com.au>
2016-10-26 20:06:30 +0800

    Fixing index

--- react/index.prod.js
+++ react/index.prod.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Routings from './base/Router';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
 
 const app = document.getElementById('page');
 
